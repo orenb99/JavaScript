b6f011458eadfb346de2e88dbd84ba6f127731a1
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-18 14:49:19 +0200

    created question 4

---
+++ question3.js
@@ -0,0 +1,10 @@
+"use strict"
+const number1=Number(prompt("Enter the first number"));
+const number2=Number(prompt("Enter the second number"));
+const number3=Number(prompt("Enter the third number"));
+let high=number1;
+let med=number1;
+let low=number1;
+
+
+alert(high + " , " + med + " , " + low);
