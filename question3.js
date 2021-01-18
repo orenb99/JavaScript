5d9cb3bbae77f570a9738e6ba46fad58e5143a7a
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-18 17:55:42 +0200

    question3 done

--- question3.js
+++ question3.js
@@ -1,10 +1,30 @@
 "use strict"
-const number1=Number(prompt("Enter the first number"));
-const number2=Number(prompt("Enter the second number"));
-const number3=Number(prompt("Enter the third number"));
-let high=number1;
-let med=number1;
-let low=number1;
-
-
-alert(high + " , " + med + " , " + low);
+const x=Number(prompt("Enter the first number"));
+const y=Number(prompt("Enter the second number"));
+const z=Number(prompt("Enter the third number"));
+if(x>y&&x>z){
+    if(y>z){
+        alert(x + " , " + y + " , " + z);
+    }
+    else{
+        alert(x + " , " + z + " , " + y);
+    }
+}
+else{
+    if(y>x&&y>z){
+        if(x>z){
+            alert(y + " , " + x + " , " + z);
+        }
+        else{
+            alert(y + " , " + z + " , " + x);
+        }
+    }
+    else{
+            if(x>y){
+                alert(z + " , " + x + " , " + y);
+            }
+            else{
+                alert(z + " , " + y + " , " + x);
+            }
+        }
+    }
