f6bccfaa1b62a30bc94ae30ed57a9c7fbc752d6e
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-18 12:41:50 +0200

    question1 done

---
+++ question1.js
@@ -0,0 +1,9 @@
+"use strict"
+const number1=Number(prompt("Enter the first number"));
+const number2=Number(prompt("Enter the second number"));
+if(number1>number2){
+    alert(number1);
+}
+else{
+    alert(number2);
+}
